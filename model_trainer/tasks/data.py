f887687c28a4311d2ee65634ed7bf08427f4e963
Hannes Hansen <mail@hannesh.de>
2023-06-02 15:14:23 +0200

    updated reqs

--- model_trainer/tasks/data.py
+++ model_trainer/tasks/data.py
@@ -1,6 +1,7 @@
 from darts.datasets import AirPassengersDataset
 from ksql import KSQLAPI
 import os
+import json
 
 class DataLoader():
     def __init__(self):
@@ -13,18 +14,18 @@ class DataLoader():
         return self.data
 
 class KafkaLoader():
-    def __init__(self, topic, mapping, filter):
+    def __init__(self, topic, mappings, inputTopics):
        self.client = KSQLAPI(os.environ['KSQL_SERVER'])
        self.stream_name = "data"
        self.topic = topic
-       self.mapping = mapping
-       self.filter = filter
+       self.mappings = json.loads(mappings)
+       self.inputTopics = json.loads(inputTopics)
 
     def build_select_query(self):
-        return f"select * from {self.stream_name}"
+        return f"select time, value from {self.stream_name}"
 
     def create_stream(self):
-        query = f"""CREATE STREAM {self.stream_name} (profileId VARCHAR, latitude DOUBLE, longitude DOUBLE)
+        query = f"""CREATE STREAM {self.stream_name} (latitude DOUBLE)
         WITH (kafka_topic='{self.topic}', value_format='json', partitions=1);"""
 
         self.client.query(query)
