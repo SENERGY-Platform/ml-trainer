7a786fa3bdba6ecc388cf7a3dd55093377008abe
Hannes Hansen <mail@hannesh.de>
2024-06-14 13:53:44 +0200

    added peak shaving endpoint

--- model_trainer/api.py
+++ model_trainer/api.py
@@ -8,6 +8,16 @@ from model_trainer.exceptions import K8sException
 config = Config()
 train_blueprint = Blueprint("api", __name__)
 
+def load_common_config_from_request():
+    request_data = request.get_json()
+    user_id = request_data("unknown_user")
+    experiment_name = request_data.get('experiment_name', "") 
+    data_settings = request_data['data_settings']
+    task_settings = request_data['task_settings']
+    ray_image = request_data['ray_image']
+    toolbox_version = request_data.get('toolbox_version', "v2.0.16")
+    return user_id, experiment_name, data_settings, task_settings, ray_image, toolbox_version
+
 @train_blueprint.route('/job/<job_id>', methods=['GET'])
 def get_task_status(job_id):
     k8s_client = KubernetesAPIClient()
@@ -27,13 +37,8 @@ def get_task_status(job_id):
 
 @train_blueprint.route('/loadshifting', methods=['POST'])
 def loadshifting():
-    user_id = "user"
-    request_data = request.get_json()
-    experiment_name = request_data['experiment_name'] 
-    data_settings = request_data['data_settings']
+    user_id, experiment_name, data_settings, task_settings, ray_image, toolbox_version = load_common_config_from_request()
     data_settings['file_name'] = experiment_name
-    ray_image = request_data['ray_image']
-    toolbox_version = request_data.get('toolbox_version', "v2.0.16")
     data_source = 's3'
     task = "load_shifting"
     ray_handler = RayKubeJobHandler()
@@ -46,14 +51,23 @@ def loadshifting():
 
 @train_blueprint.route('/mlfit', methods=['POST'])
 def anomaly():
-    user_id = "user"
+    user_id, experiment_name, data_settings, task_settings, ray_image, toolbox_version = load_common_config_from_request()
     request_data = request.get_json()
-    experiment_name = request_data.get('experiment_name', "") 
-    data_settings = request_data['data_settings']
-    task_settings = request_data['task_settings']
-    ray_image = request_data['ray_image']
-    toolbox_version = request_data.get('toolbox_version', "v2.0.16")
     task = "ml_fit"
+    data_source = request_data['data_source']
+    ray_handler = RayKubeJobHandler()
+    try:
+        task_id = ray_handler.start_job(task, task_settings, user_id, experiment_name, data_settings, ray_image, toolbox_version, data_source)
+        return jsonify({'task_id': str(task_id), 'status': 'Processing'})
+    except Exception as e:
+        current_app.logger.error("Could not start job: " + str(e))
+        return jsonify({'error': 'could not start job', 'message': str(e)}), 500
+
+@train_blueprint.route('/peak_shaving', methods=['POST'])
+def anomaly():
+    user_id, experiment_name, data_settings, task_settings, ray_image, toolbox_version = load_common_config_from_request()
+    request_data = request.get_json()
+    task = "peak_shaving"
     data_source = request_data['data_source']
     ray_handler = RayKubeJobHandler()
     try:
---
+++ model_trainer/schema.py
@@ -0,0 +1,19 @@
+from typing import Literal
+
+
+class KafkaSettings(BaseModel):
+    name: str 
+    path_to_time: str
+    path_to_value: str 
+    filterType: Literal['device_id', 'operator_id', 'import_id']
+    filterValue: str
+    ksql_url: str 
+    timestamp_format: str
+    time_range_value: float
+    time_range_level: str
+
+class Job(BaseModel):
+    data_source: Literal['kafka', 's3']
+    data_settings: KafkaSettings
+    toolbox_version: str
+    ray_image: str 
